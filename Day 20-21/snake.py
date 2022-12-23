e65db8bd34fcad708a1245530d43a39a945cd87c
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2022-12-23 13:26:59 -0500

    deleted general main file and created the blocks that will be used as the starting shape of the snake

---
+++ Day 20-21/snake.py
@@ -0,0 +1,18 @@
+import turtle as t
+
+screen = t.Screen()
+screen.bgcolor("black")
+
+b1 = t.Turtle()
+b1.color("green")
+b1.shape("square")
+
+b2 = t.Turtle()
+b2.color("green")
+b2.shape("square")
+
+b3 = t.Turtle()
+b3.color("green")
+b3.shape("square")
+
+screen.exitonclick()
