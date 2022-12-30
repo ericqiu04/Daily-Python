afda3fb90b9df106a834cd7e8952d8327a0deda1
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2022-12-30 15:20:45 -0500

    finished creating movement for snake project

---
+++ Day 20-21/game.py
@@ -0,0 +1,29 @@
+import turtle as t
+import time
+from snake import Snake
+
+screen = t.Screen()
+screen.setup(width = 1000, height = 1000)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.update()
+game_is_on = True
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+
+
+screen.exitonclick()
--- Day 20-21/snake.py
+++ Day 20-21/snake.py
@@ -1,27 +1,47 @@
 import turtle as t
+STARTING = [(-20,0), (0,0), (20,0)]
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
 
-screen = t.Screen()
-screen.setup(width = 1000, height = 1000)
-screen.bgcolor("black")
-screen.title("Snake Game")
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[len(self.segments) - 1]
+
+    def create_snake(self):
+        for position in STARTING:
+            new_segment = t.Turtle()
+            new_segment.shape("square")
+            new_segment.color("light green")
+            new_segment.penup()
+            new_segment.setpos(position)
+            self.segments.append(new_segment)
+
+    def move(self):
+        for s in range(0, len(self.segments) - 1, 1):
+            newx = self.segments[s + 1].xcor()
+            newy = self.segments[s + 1].ycor()
+            self.segments[s].goto(newx, newy)
+        self.head.forward(20)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
 
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
-starting_positions = [(-20,0), (0,0), (20,0)]
 
-segments = []
-for position in starting_positions:
-    new_segment = t.Turtle()
-    new_segment.shape("square")
-    new_segment.color("light green")
-    new_segment.penup()
-    new_segment.setpos(position)
-    segments.append(new_segment)
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
 
-game_is_on = True
 
-while game_is_on:
-    for s in segments:
-        s.forward(10)
-
-screen.exitonclick()
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
