3f17368bca77e5a0beca5c5f87d0ede84061b347
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2022-12-30 17:11:01 -0500

    finished with score and display game over text

--- Day 20-21/game.py
+++ Day 20-21/game.py
@@ -2,38 +2,59 @@ import turtle as t
 import time
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 
-score = 0
+#creating objects
+snake = Snake()
+food = Food()
+score = Scoreboard()
+
+#screen
 screen = t.Screen()
 screen.setup(width = 1000, height = 1000)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-snake = Snake()
-food = Food()
+
+#score
+score.color("white")
+score.penup()
+score.goto(0, 450)
+score.hideturtle()
+
 
 screen.update()
 game_is_on = True
 
+#movement listener
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down,"Down")
 screen.onkey(snake.right, "Right")
 screen.onkey(snake.left, "Left")
 
+#game
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-
+    score.penup()
     #see if touching
     if snake.head.distance(food) < 20:
         food.hideturtle()
-        score +=1
+        score.addScore()
         del food
         print("nom nom")
         food = Food()
+        score.clear()
+        screen.title(score.getScore())
+
+    #collision with walls
+    if snake.head.xcor() >480 or snake.head.xcor() < -480 or snake.head.ycor() >480 or snake.head.ycor() < -480:
+        game_is_on = False
+        score.gameOver()
+
 
 
 
---
+++ Day 20-21/scoreboard.py
@@ -0,0 +1,16 @@
+from turtle import Turtle
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+
+    def addScore(self):
+        self.score +=1
+
+    def getScore(self):
+        return "Score = " + str(self.score)
+
+    def gameOver(self):
+        self.goto(0,0)
+        self.write("GAME OVER", align = "center", font = ('Arial', 24, 'bold'))
